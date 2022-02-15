ce4e3e321f61912cc75ee0d335747c926586f760
Matheus Andrade <93740980+MatheusAndradeMelo@users.noreply.github.com>
2022-02-14 22:50:42 -0300

    iniciando banco de dados sqllite,sqlalchemy, implementando tratamentos de erros, e correções nos metodos get,post,put,delete

--- app.py
+++ app.py
@@ -8,12 +8,22 @@ app = Flask(__name__)
 #faz todo geranciamento da api
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db' # pode substituir o caminho para outros bancos como postgresql por ex
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # para parar de ficar dando aviso
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all
+
 # adicionando o recurso para a api
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
 # configuração básica do flask
 if __name__ == 'main':
+    # conexão com o banco feita nessa condição para que não venha ter um arquivo c outro nome, somente o arquivo principal
+    from sql_alchemy import banco
+    banco.init_app(app)
     # após finalizar a api mudar pra false ou retirar
     app.run(debug=True)
     
--- resources/hotel.py
+++ resources/hotel.py
@@ -2,87 +2,69 @@ from flask_restful import Resource, reqparse
 
 from models.HotelModel import HotelModel
 
-#lista de hoteis
-hoteis = [
-    {
-    'hotel_id': 'alpha',
-    'nome': 'Alpha Hotel',
-    'estrelas': 4.3,
-    'diaria': 420.34,
-    'cidade': 'Rio de janeiro'
-    },
-    {
-    'hotel_id': 'bravo',
-    'nome': 'Bravo Hotel',
-    'estrelas': 4.4,
-    'diaria': 380.90,
-    'cidade': 'Teresópolis'
-    },
-    {
-    'hotel_id': 'charlie',
-    'nome': 'Charlie Hotel',
-    'estrelas': 3.9,
-    'diaria': 320.20,
-    'cidade': 'Niterói'
-    },
-    
-    ]
-
 # recurso da api
 class Hoteis(Resource):
-    
-    # requisicao pra leitura
+    # requisicao pra leitura dos hoteis
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]} # Select * from Hoteis
     
 class Hotel(Resource):
     # selecionando argumentos especificos para aceitar na requisição
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
-    
-    # verifica se o hotel já existe
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return print('Hotel não existe.')
+    argumentos.add_argument('nome', type=str, required=True, help="This field 'nome' cannot be left blank")
+    argumentos.add_argument('estrelas', type=float, required=True, help="This field 'estrelas' cannot be left blank")
+    argumentos.add_argument('diaria', type=float, required=True, help="This field 'diaria' cannot be left blank")
+    argumentos.add_argument('cidade', type=str, required=True, help="This field 'cidade' cannot be left blank")
     
     # método get para mostrar um hotel
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Hotel not found.'}, 404 # not found
     
     # método post pra inserir um novo hotel
     def post(self, hotel_id):
+        
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 #Bad request
+        
         # adicionando um construtor
         dados = Hotel.argumentos.parse_args()
         # criando um novo hotel
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        # adicionando o novo hotel na lista de hoteis
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201 #Created
-    
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500 #Internal server error
+        return hotel.json()
+        
+        
     # método para atualizar um hotel
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200 #Ok
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201 #Created
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200 #Ok
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500 #Internal server error
+        return hotel.json(), 201 #Created
     
     # método para deletar um hotel
     def delete(self, hotel_id):
         # global para o python não achar que a várivael hoteis foi criada neste momento e já está sendo usada
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deleted.'}
+        #global hoteis
+       hotel = HotelModel.find_hotel(hotel_id)
+       if hotel:
+           try:
+                hotel.delete_hotel()
+           except:
+               return {'message': 'An error ocurred trying to delete hotel.'}, 500
+                
+           return {'message': 'Hotel deleted.'}
+       return {'message': 'Hotel not found.'}, 404
